9f14563703f0151959290705a7d194a562f91602
Robert Oprescu <robert.oprescu_ext@sonepar.com>
2024-11-22 19:43:07 +0200

    commit

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,10 +2,10 @@
     <div class="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 m-3">
         <a :href="product.product_url" target="_blank" v-for="product in products" :key="product.pinecone_id" class="flex flex-col h-[300px] rounded-2xl shadow-2xl p-3 border gap-2">
             <img :src="product.img_url" alt="product image" class="rounded-lg w-[150px] self-center"/>
-            <p class="text-black text-md font-bold text-center">{{ product.title }}</p>
-            <p class="text-black text-sm text-center">{{ product.stars }} stars</p>
-            <p class="text-black text-sm text-center">{{ product.reviews }} reviews</p>
-            <p class="text-black text-sm text-center">{{ product.price }} $</p>
+            <p class="text-black text-xs text-center">{{ product.title }}</p>
+            <p class="text-black text-xs text-center">{{ product.stars }} stars</p>
+            <p class="text-black text-xs text-center">{{ product.reviews }} reviews</p>
+            <p class="text-black text-xs text-center">{{ product.price }} $</p>
         </a>
         <div v-if="!products.length" class="self-center justify-self-center">
             <p class="text-black text-2xl font-bold text-center my-4 justify-self-center self-center">No products found</p>
